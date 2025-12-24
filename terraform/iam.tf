c35910038d159cfabd202afedcc549407fdc9809
James King <ripixel@gmail.com>
2025-12-24 22:58:21 +0000

    feat: update CircleCI OIDC binding to use project number and grant service account token creator role.

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -19,22 +19,24 @@ resource "google_project_iam_member" "circleci_storage_admin" {
   member  = "serviceAccount:${google_service_account.circleci_web_deployer.email}"
 }
 
-# Workload Identity Pool binding for CircleCI OIDC
-# Note: This uses the existing workload identity pool created by the server repo
-data "google_iam_workload_identity_pool" "circleci_pool" {
-  workload_identity_pool_id = "circleci-pool"
-}
-
-data "google_iam_workload_identity_pool_provider" "circleci_provider" {
-  workload_identity_pool_id          = data.google_iam_workload_identity_pool.circleci_pool.workload_identity_pool_id
-  workload_identity_pool_provider_id = "circleci-provider"
+# Get project number for workload identity pool path
+data "google_project" "project" {
+  project_id = var.project_id
 }
 
 # Allow the CircleCI OIDC tokens to impersonate the web deployer service account
-# Note: Uses the same attribute filter as the server deployer (CircleCI org ID)
-# Both server and web repos can use the same workload identity pool
+# Note: Uses wildcard pattern to match the server repo configuration
+# The workload identity pool is created and managed by the server repo
 resource "google_service_account_iam_member" "circleci_web_workload_identity" {
   service_account_id = google_service_account.circleci_web_deployer.name
   role               = "roles/iam.workloadIdentityUser"
-  member             = "principalSet://iam.googleapis.com/${data.google_iam_workload_identity_pool.circleci_pool.name}/attribute.project_id/b2fc92f7-4f8d-4676-95b1-94d7f15c0a8e"
+  member             = "principalSet://iam.googleapis.com/projects/${data.google_project.project.number}/locations/global/workloadIdentityPools/circleci-pool/*"
+}
+
+# Grant Token Creator permission to the workload identity pool
+# This allows the pool to generate access tokens for the service account
+resource "google_service_account_iam_member" "circleci_web_token_creator" {
+  service_account_id = google_service_account.circleci_web_deployer.name
+  role               = "roles/iam.serviceAccountTokenCreator"
+  member             = "principalSet://iam.googleapis.com/projects/${data.google_project.project.number}/locations/global/workloadIdentityPools/circleci-pool/*"
 }
