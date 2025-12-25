42f78260927c9ac4bc653b8e3aa4994e3175bfe7
James King <ripixel@gmail.com>
2025-12-25 11:57:38 +0000

    docs: Remove `google_firebase_hosting_site` resource and add comments explaining Firebase CLI site creation.

--- terraform/firebase.tf
+++ terraform/firebase.tf
@@ -1,6 +1,10 @@
-# Firebase Hosting site
-# Note: This creates the hosting site. The actual content deployment is handled by Firebase CLI.
-resource "google_firebase_hosting_site" "default" {
-  project = var.project_id
-  site_id = var.project_id
-}
+# Firebase Hosting Site
+#
+# Note: Firebase Hosting sites are created automatically on first deployment via Firebase CLI.
+# There is no Terraform resource for google_firebase_hosting_site in the Google provider.
+#
+# The site will be created when CircleCI runs:
+#   firebase deploy --only hosting --project fitglue-server-{env}
+#
+# If you need to pre-create the site manually, use:
+#   firebase hosting:sites:create {site-id} --project {project-id}
